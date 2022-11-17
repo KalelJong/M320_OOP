aeca665f31b2726eaed71f39768b87c8dea4a1e8
Kalel Poeta <kalel.jong@edu.tbz.ch>
2022-11-17 09:45:17 +0100

    Created menu to run different programs

--- src/ch/gugus/Main.java
+++ src/ch/gugus/Main.java
@@ -1,8 +1,23 @@
 package ch.gugus;
 
+import ch.gugus.d1_thermostat.D1;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choose program to run");
+        String programToRun = scan.nextLine();
+        switch (programToRun)
+        {
+            case "D1":
+                new D1().run();
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/ch/gugus/d1_thermostat/D1.java
+++ src/ch/gugus/d1_thermostat/D1.java
@@ -1,4 +1,8 @@
 package ch.gugus.d1_thermostat;
 
 public class D1 {
+
+    public void run(){
+        System.out.println("Hello");
+    }
 }
