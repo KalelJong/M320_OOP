f7e22d612a3de0400c1266ce3c74417d19f0e964
Kalel Poeta <kalel.jong@edu.tbz.ch>
2022-11-17 11:15:48 +0100

    Update main

--- src/ch/gugus/Main.java
+++ src/ch/gugus/Main.java
@@ -3,21 +3,27 @@ package ch.gugus;
 import ch.gugus.d1_thermostat.D1;
 
 import java.io.InputStream;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Choose program to run");
-        String programToRun = scan.nextLine();
-        switch (programToRun)
-        {
-            case "D1":
-                new D1().run();
-                break;
-            default:
-                break;
+        while (true){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Choose program to run");
+            String programToRun = scan.nextLine();
+            switch (programToRun.toLowerCase())
+            {
+                case "d1":
+                    new D1().run();
+                    break;
+                case "q":
+                    return;
+                default:
+                    break;
+            }
         }
+
     }
 }
