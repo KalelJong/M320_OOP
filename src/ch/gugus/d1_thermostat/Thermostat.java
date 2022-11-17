7ce010d47019a30722952e9280cb9e44c6a155b9
Zakria Ahmed Samma <zakria.samma@edu.tbz.ch>
2022-11-17 10:27:48 +0100

    D1 Task b) Class

--- src/ch/gugus/d1_thermostat/Thermostat.java
+++ src/ch/gugus/d1_thermostat/Thermostat.java
@@ -1,4 +1,60 @@
 package ch.gugus.d1_thermostat;
 
 public class Thermostat {
+    private int temperature;
+    private int min;
+    private int max;
+    private int increment;
+
+    public Thermostat(int min, int max) {
+        this.temperature = 15;
+        this.min = min;
+        this.max = max;
+        this.increment = 0;
+    }
+
+    public Thermostat(int temperature, int min, int max) {
+        this.temperature = temperature;
+        this.min = min;
+        this.max = max;
+        this.increment = 0;
+    }
+
+    public void changeTemperature(int increment){
+        if (!(temperature + increment > max || temperature + increment < min)) {
+            this.temperature += increment;
+        }
+    }
+
+    public int getIncrement() {
+        return increment;
+    }
+
+    public void setIncrement(int increment) {
+        this.increment = increment;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
 }
